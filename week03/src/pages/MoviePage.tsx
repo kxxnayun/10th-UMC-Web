0995f8903c20fe02565c17c094abc42e2ef8c3ee
kxxnayun <nayun2179@gmail.com>
2026-04-01 10:55:08 +0900

    chore: 초기 파일 세팅

--- week03/src/pages/MoviePage.tsx
+++ week03/src/pages/MoviePage.tsx
@@ -11,7 +11,7 @@ const MoviesPage = () => {
         "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
         {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZmExOTAwM2E2MDgzNDUzYzUzMWVjODE1ZWVlNzhiMCIsIm5iZiI6MTc1OTAzODM3NS42MDgsInN1YiI6IjY4ZDhjYmE3OGJiYWY0MzQ0YzM4YjkzMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cz9nxau3Diw43E-T1fdzrlTT5dZSR_a5RLcgl8eeKyk`, // 본인 TMDB 토큰으로 교체
+            Authorization: `Bearer 토큰`,
           },
         },
       );
