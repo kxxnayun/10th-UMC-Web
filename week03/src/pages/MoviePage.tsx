326f3fcd530dd668515c3cd051374bee7f3e9e6a
kxxnayun <nayun2179@gmail.com>
2026-04-01 17:42:48 +0900

    feat: 로딩 스피너 추가

--- week03/src/pages/MoviePage.tsx
+++ week03/src/pages/MoviePage.tsx
@@ -2,13 +2,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import type { Movie, MovieResponse } from "../types/movie";
 import MovieItem from "../components/MovieItem";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   const TMBD_TOKEN = import.meta.env.VITE_TMDB_KEY;
 
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
+    setIsLoading(true);
+
     const fetchMovies = async () => {
       const { data } = await axios.get<MovieResponse>(
         "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
@@ -24,8 +29,9 @@ const MoviesPage = () => {
     fetchMovies();
   }, []);
 
-  console.log("render start");
-  console.log(movies);
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <div className="p-10">
